e4c9ada9aa7d8bceea0a707b1219454d7cde7fd2
WebDevGirl <thedpq@gmail.com>
2013-02-10 12:59:08 -0800

    Update Footer

--- html/includes/foot.php
+++ html/includes/foot.php
@@ -4,8 +4,9 @@
 			<a href="?logout=true">Logout</a>
 		<?php
 			} else {
-			  $authUrl = $client->createAuthUrl();
-			  print "<a class='login' href='$authUrl'>Connect Me!</a>";
+				$client = new Google_Client();
+				$authUrl = $client->createAuthUrl();
+				print "<a class='login' href='$authUrl'>Connect Me!</a>";
 			} ?>
 		</footer>
 		
