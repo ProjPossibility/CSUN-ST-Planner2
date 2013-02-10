9e6402d8f7bfabb1b95d6333bd454e3c28189678
WebDevGirl <thedpq@gmail.com>
2013-02-10 11:34:12 -0800

    handle success and error.

--- html/teacher.php
+++ html/teacher.php
@@ -7,6 +7,7 @@
 	require($prefix."/includes/vars.php");	
 	require($prefix."/includes/head.php");
 
+	$success_msg = (isset($_GET["success_msg"]))?$_GET["success_msg"]:"";
 
 	$client = new Google_Client();
 	$client->setUseObjects(true); 
@@ -20,9 +21,62 @@
 	}
 
 	//-- All Systems's Go
-	if ($client->getAccessToken()) { ?>
+	if ($client->getAccessToken()) { 
+		$optParams = array(
+					'minAccessRole' => "owner"
+					);
 
-		This is the test.
+
+
+		$calList = $cal->calendarList->listCalendarList($optParams);
+		//print("<pre>");
+		//print_r(get_class_methods("Google_CalendarListEntry"));
+		//print("</pre>");
+
+		/*
+		foreach($calList["items"] as $calendar) {
+			echo $calendar->getSummary();
+		}
+
+		print "<h1>Calendar List</h1><pre>" . print_r($calList, true) . "</pre>";?>
+		*/
+		?>
+	
+		<form name="input" action="teacher.do.php" method="post">
+        <fieldset>
+          <legend>Add New Student</legend>
+          <?php 
+		 	if(isset($success_msg) && !empty($success_msg)) {
+				echo "<div class=\"alert alert-success\">";
+				echo $success_msg;
+				echo "</div>";
+		 	} ?>
+		 	<?php if(isset($errors)) {?>
+			<div class="alert alert-error">
+				<p><b>Alert:</b> Please address the following issues:</p>
+				<?php if(isset($errors)) {displayAllErrors($errors);}?>
+			</div>
+			<?php } ?>
+          <div class="control-group">
+            <label class="control-label" for="student_name">Student's Name</label>
+            <div class="controls">
+              <input type="text" class="input-xlarge" id="student_name" name="student_name">
+              <p class="help-block"></p>
+            </div>
+          </div>
+           <div class="control-group">
+            <label class="control-label" for="student_email">Student's Email</label>
+            <div class="controls">
+              <input type="text" class="input-xlarge" placeholder="examples@gmail.com" id="student_email" name="student_email">
+              <p class="help-block">Calendar will be shared with the student via this email</p>
+            </div>
+          </div>
+          <div class="form-actions">
+            <button type="submit" class="btn btn-primary">Add New Student</button>
+            <button type="reset" class="btn">Cancel</button>
+          </div>
+        </fieldset>
+      </form>
 
 	<?php } ?>	
 
