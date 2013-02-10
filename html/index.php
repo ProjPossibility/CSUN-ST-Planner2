2e41445270e1cc1ccde1d17f9f74f60ead1545af
WebDevGirl <thedpq@gmail.com>
2013-02-10 15:28:17 -0800

    Add index to template

---
+++ html/index.php
@@ -0,0 +1,37 @@
+<?php 
+	$prefix = ".";
+	$section = "root";
+	$page = "teacher";
+	$subtitle = "Teacher's Dashboard";
+	
+	require_once($prefix."/includes/vars.php");	
+	require_once($prefix."/includes/head.php");
+	
+	?>
+		<h2>Select Role:</h2>
+		<ul class="thumbnails">
+			<li class="span2 text-center">
+			<a href="student.php">
+			<div class="thumbnail">
+				<object data="img/student.svg" width="128" height="150" type="image/svg+xml">
+					<img src="img/student.png" width="128" height="150" alt="Student"/>
+				</object>
+				<h3>Student</h3>
+			</div>
+			</a>
+			</li>
+			<li class="span2 text-center">
+			<a href="teacher.php">
+			<div class="thumbnail">
+				<object data="img/teacher.svg" width="" height="150"  type="image/svg+xml">
+					<img src="img/teacher.png" width="128" height="119" alt="Teacher"/>
+				</object>
+				<h3>Teacher</h3>
+			</div>
+			</a>
+			</li>
+		</ul>
+		
+<?php
+	require($prefix."/includes/foot.php");
+?>
