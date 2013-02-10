b55962f6b3a13c488618815e32b08743281f4c95
WebDevGirl <thedpq@gmail.com>
2013-02-09 17:14:31 -0800

    Add Teacher Student View

---
+++ html/teacher_student_view.php
@@ -0,0 +1,16 @@
+<?php 
+	$prefix = ".";
+	$section = "root";
+	$page = "teacher_student_view";
+	$subtitle = "Teacher Student View";
+	
+	require($prefix."/includes/vars.php");	
+	require($prefix."/includes/head.php");
+
+?>	
+
+Teacher Student View
+
+<?php
+	require($prefix."/includes/foot.php");
+?>
