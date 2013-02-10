3db6c824a430c6655b15559630a775ea7b9437ac
WebDevGirl <thedpq@gmail.com>
2013-02-10 10:35:12 -0800

    Update footer to only prompt logout when logged in.

--- html/includes/foot.php
+++ html/includes/foot.php
@@ -1,4 +1,10 @@
-		<a href="?logout=true">Logout</a>
+		<footer>
+		<?php
+		if (method_exists($client,"getAccessToken") && $client->getAccessToken())  { ?>
+			<a href="?logout=true">Logout</a>
+		<?php } ?>
+		</footer>
+		
 		</div>
 
         <script src="http://code.jquery.com/jquery.js"></script>
