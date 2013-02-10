1cba551979869e6b36938b6c18f63cb8b280f06d
WebDevGirl <thedpq@gmail.com>
2013-02-09 17:06:33 -0800

    add header

--- html/includes/head.php
+++ html/includes/head.php
@@ -1,1 +1,11 @@
-- header -<br><Br>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Add tasks and milestones</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+
+    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+  	<div class="page" style="max-width:550px;">
