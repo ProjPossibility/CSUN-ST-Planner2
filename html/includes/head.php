eb02af1e7651f86a5af0818c51db580ed6d1eb9f
WebDevGirl <thedpq@gmail.com>
2013-02-10 16:34:23 -0800

    Fix page title

--- html/includes/head.php
+++ html/includes/head.php
@@ -4,7 +4,7 @@
 <html>
     
     <head>
-        <title>Add tasks and milestones</title>
+        <title>CSUN-ST-Planner2 <?php echo (isset($subtutle))?' - '.$subtitle:""; ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Bootstrap -->
         <link href="css/bootstrap.css" rel="stylesheet" media="screen">
